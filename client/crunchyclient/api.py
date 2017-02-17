1e1f076f4da2c229bf7350a8088e472b7d484ae9
Arjen Meek <arjen@meeknet.nl>
2017-02-17 15:47:20 +0000

    client: Create StatementAPI wrapper

--- client/crunchyclient/api.py
+++ client/crunchyclient/api.py
@@ -5,6 +5,7 @@ import requests
 from requests.exceptions import HTTPError
 
 from crunchylib.exceptions import NotFoundError
+from crunchylib.utility import deserialize_value, serialize_value
 
 
 class API(object):
@@ -41,3 +42,20 @@ class API(object):
         r = requests.put('{}/{}'.format(self.url, path), data=json.dumps(data))
         self._raise_from_request(r)
         return r.json()
+
+
+class StatementAPI(API):
+
+    def _process_raw_statement(self, raw_statement):
+        elements = [deserialize_value(v) for v in raw_statement]
+        return elements
+
+    def get_statement(self, uuid_):
+        raw_statement = self.get('statements/{}'.format(serialize_value(uuid_)))
+        elements = [deserialize_value(v) for v in raw_statement]
+        return elements
+
+    def find_statements(self):
+        raw_statements = self.get('statements')
+        processed_statements = [self._process_raw_statement(s) for s in raw_statements]
+        return processed_statements
