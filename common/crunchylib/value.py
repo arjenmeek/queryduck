dc53bc295dd5e9b204aca6fa847b006a8dcd74fe
Arjen Meek <arjen@meeknet.nl>
2020-03-28 16:55:07 +0000

    common: Expand and improve Statement and Value classes

--- common/crunchylib/value.py
+++ common/crunchylib/value.py
@@ -20,41 +20,51 @@ class Statement:
                 data[k].append(v.serialize())
         return data
 
+    def __repr__(self):
+        return '<Statement id={} uuid={}>'.format(self.id, self.uuid)
+
 
 class Value(object):
 
     value_types = {
         'int': {
+            'type': int,
             'factory': int,
             'column_name': 'object_integer',
             'serializer': str,
         },
         'bool': {
+            'type': bool,
             'factory': bool,
             'column_name': 'object_boolean',
             'serializer': str,
         },
         'float': {
+            'type': float,
             'factory': float,
             'column_name': 'object_float',
             'serializer': str,
         },
         'str': {
+            'type': str,
             'factory': str,
             'column_name': 'object_string',
             'serializer': str,
         },
         'datetime': {
+            'type': datetime.datetime,
             'factory': lambda dt: datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.%f'),
             'column_name': 'object_datetime',
             'serializer': lambda dt: dt.strftime('%Y-%m-%dT%H:%M:%S.%f'),
         },
         'st': {
+            'type': Statement,
             'factory': Statement,
             'column_name': 'object_statement_id',
             'serializer': lambda st: st.uuid,
         },
         'none': {
+            'type': type(None),
             'factory': lambda x: None,
             'column_name': 'id', # for "IS NULL" comparison
             'serializer': str,
@@ -75,33 +85,47 @@ class Value(object):
 
     def __init__(self, serialized=None, native=None, db_columns=None, db_row=None, db_entities=None):
         if serialized is not None:
-            self.vtype, ser_content = serialized.split(':', 1)
+            self.vtype, ser_v = serialized.split(':', 1)
             options = self.value_types[self.vtype]
             self.column_name = options['column_name']
             self.serializer = options['serializer']
-            self.content = options['factory'](ser_content)
+            self.v = options['factory'](ser_v)
+        elif native is not None:
+            for vt, options in self.value_types.items():
+                if options['type'] == type(native):
+                    self.vtype = vt
+                    self.column_name = options['column_name']
+                    self.serializer = options['serializer']
+                    self.v = native
+                    break
         elif db_columns is not None and db_row is not None:
             for vtype, options in self.value_types.items():
                 column = db_columns[options['column_name']]
-                content = db_row[column]
-                if content is None or vtype == 'none':
+                v = db_row[column]
+                if v is None or vtype == 'none':
                     continue
                 self.vtype = vtype
                 self.column_name = options['column_name']
                 self.serializer = options['serializer']
                 if vtype == 'st':
                     uuid_ = db_row[db_entities['st'].c.uuid]
-                    self.content = Statement(uuid_=uuid_, id_=content)
+                    self.v = Statement(uuid_=uuid_, id_=v)
                 else:
-                    self.content = content
+                    self.v = v
                 break
 
+    @classmethod
+    def native(cls, native_value):
+        return cls(native=native_value)
+
+    def __repr__(self):
+        return '<Value type={} v={}>'.format(self.vtype, self.v)
 
     def db_value(self):
         if self.vtype == 'st':
-            return self.content.id
+            return self.v.id
         else:
-            return self.content
+            return self.v
 
     def column_compare(self, op, columns):
         column = columns[self.column_name]
@@ -109,4 +133,4 @@ class Value(object):
         return getattr(column, op_method)(self.db_value())
 
     def serialize(self):
-        return '{}:{}'.format(self.vtype, self.serializer(self.content))
+        return '{}:{}'.format(self.vtype, self.serializer(self.v))
