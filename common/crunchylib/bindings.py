f4d1fbe55d413a2b33dd5608a5d1f255a418f678
Arjen Meek <arjen@meeknet.nl>
2020-07-10 18:30:02 +0000

    common: Add new Bindings class to bind Schema elements to program code
    This replaces the functionality that was previously done using Schema

---
+++ common/crunchylib/bindings.py
@@ -0,0 +1,23 @@
+from .exceptions import CVSchemaError
+
+
+class Bindings:
+
+    def __init__(self, content):
+        self._content = content
+
+    def __getitem__(self, attr):
+        if not attr in self._content:
+            raise CVSchemaError(
+                "Key is not part of these Bindings: {}".format(attr))
+        return self._content[attr]
+
+    def __getattr__(self, attr):
+        return self._content[attr]
+
+    def reverse(self, statement):
+        for k, v in self._content.items():
+            if v == statement:
+                return k
+        else:
+            return None
