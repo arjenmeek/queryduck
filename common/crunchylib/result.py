8c5f5b33da1f4180e849c2e7a7cbb80b00bce90c
Arjen Meek <arjen@meeknet.nl>
2020-07-10 18:31:37 +0000

    common: Various changes to result-related classes
    - StatementSet was renamed to Result, and is more limited in scope.
    - ResultSet was removed altogether.

--- common/crunchylib/result.py
+++ common/crunchylib/result.py
@@ -1,40 +1,13 @@
 from .types import Statement, Blob, deserialize
 
 
-class StatementSet:
+class Result:
 
-    def __init__(self, statements=None):
-        self.statements = {}
-        self.blobs = {}
-        if statements:
-            self.add(statements)
-
-    def add(self, statements):
-        for ref, info in statements.items():
-            s = self.unique_deserialize(ref)
-            s.triple = tuple(self.unique_deserialize(r) for r in info)
-
-    def unique_deserialize(self, ref):
-        """Ensures there is only ever one instance of the same Statement present"""
-        s = deserialize(ref)
-        if type(s) == Statement:
-            if s.uuid not in self.statements:
-                self.statements[s.uuid] = s
-            return self.statements[s.uuid]
-        elif type(s) == Blob:
-            if s.sha256 not in self.blobs:
-                self.blobs[s.sha256] = s
-            elif s.volume and not self.blobs[s.sha256].volume:
-                self.blobs[s.sha256].volume = s.volume
-                self.blobs[s.sha256].path = s.path
-            return self.blobs[s.sha256]
-        else:
-            return s
+    def __init__(self, statements, values):
+        self.statements = statements
+        self.values = values
 
     def get(self, uuid_):
-        if not uuid_ in self.statements:
-            s = Statement(uuid_=uuid_)
-            self.statements[uuid_] = s
         return self.statements[uuid_]
 
     def find(self, s=None, p=None, o=None):
@@ -47,19 +20,8 @@ class StatementSet:
                 statements.append(st)
         return statements
 
-    def get_statement_attribute(self, statement, predicate):
-        return [s.triple[2] for s in self.find(s=statement, p=predicate)]
+    def objects_for(self, subject, predicate):
+        return [s.triple[2] for s in self.find(s=subject, p=predicate)]
 
-
-class ResultSet:
-
-    def __init__(self, references, sts):
-        self.sts = sts
-        self.result_uuids = [deserialize(r).uuid for r in references]
-
-    def getall(self):
-        statements = []
-        for uuid_ in self.result_uuids:
-            s = self.sts.get(uuid_)
-            statements.append(s)
-        return statements
+    def subjects_for(self, object_, predicate):
+        return [s.triple[0] for s in self.find(p=predicate, o=object_)]
