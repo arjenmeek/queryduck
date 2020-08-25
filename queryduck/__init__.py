c1d66641a2861795256d6aa4e8c29ecb03497187
Arjen Meek <arjen@meeknet.nl>
2020-08-25 19:49:46 +0000

    Add ability to handle locally configured schemas

--- queryduck/__init__.py
+++ queryduck/__init__.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from os.path import dirname, join as pjoin
+from os.path import dirname, expanduser, join as pjoin
 
 from .connection import Connection
 from .constants import DEFAULT_SCHEMA_FILES
@@ -9,11 +9,15 @@ from .repository import StatementRepository
 
 class QueryDuck:
 
-    def __init__(self, url, username, password):
+    def __init__(self, url, username, password, extra_schema_files=None):
         self.main_dir = dirname(dirname(__file__))
         self.conn = Connection(url, username, password)
         self.repo = None
         self.bindings = None
+        if extra_schema_files is not None:
+            self.extra_schema_files = extra_schema_files
+        else:
+            self.extra_schema_files = []
 
     def get_repo(self):
         if self.repo is None:
@@ -23,11 +27,17 @@ class QueryDuck:
     def get_bindings(self):
         if self.bindings is None:
             schemas = []
-            for filename in DEFAULT_SCHEMA_FILES:
-                filepath = pjoin(self.main_dir, 'schemas', filename)
+            for filename in DEFAULT_SCHEMA_FILES + self.extra_schema_files:
+                if '/' in filename:
+                    filepath = expanduser(filename)
+                else:
+                    filepath = pjoin(self.main_dir, 'schemas', filename)
+
                 with open(filepath, 'r') as f:
                     schemas.append(json.load(f))
+
             repo = self.get_repo()
             self.bindings = repo.bindings_from_schemas(schemas)
+
         return self.bindings
 
