3d5cbf258c83fe675dfcf8ce335fc820cfc58ce2
Arjen Meek <arjen@meeknet.nl>
2020-09-06 23:39:47 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="QueryDuck",
+    version="0.5.1.0",
+    author="Arjen Meek",
+    author_email="arjen@meeknet.nl",
+    description="The QueryDuck project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arjenmeek/queryduck",
+    packages=['queryduck'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.7',
+)
