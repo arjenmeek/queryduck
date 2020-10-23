2bed732c3c5013d14509b3980502535e0b2a26d0
Arjen Meek <arjen@meeknet.nl>
2020-10-23 13:02:50 +0000

    Add "Component" enum to represent Statement components

--- queryduck/constants.py
+++ queryduck/constants.py
@@ -1,5 +1,14 @@
+import enum
+
 DEFAULT_SCHEMA_FILES = [
     "main_schema.json",
     "transaction_schema.json",
     "storage_schema.json",
 ]
+
+
+class Component(enum.Enum):
+    SELF = enum.auto
+    SUBJECT = enum.auto
+    PREDICATE = enum.auto
+    OBJECT = enum.auto
